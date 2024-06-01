d0b5b9f667ec3228a45242fed7fb3e02eeafd50e
Markk <you@example.com>
2024-06-01 14:11:35 +0300

    Week7

---
+++ ArgumentsAndParmameters/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+PrintAnyWord(userInput);  // see on sõna mida kasutaja sisestab
+static void PrintAnyWord(string anyString) // anyString on lihtsalt seletus
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("hello");
+    }
+}
---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine("Provide total:");
+int userInput = Int32.Parse(Console.ReadLine());
+double discount = CalculateDiscount(userInput);
+Console.WriteLine($"Your discount is: {discount}%");
+double newTotal = CalculateNewTotal(userInput, discount);
+Console.WriteLine($"your updated total with{discount}%: {newTotal}"); newTotal = newTotal / 100;
+static double CalculateDiscount(int total)
+{
+    if (total < 10)
+    {
+        return 1;
+    }
+    else if (total >= 10 && total < 20)
+    {
+        return 5;
+    }
+    else
+    {
+        return 10;
+    }
+}
+static double CalculateNewTotal(double total, double discount)
+{
+    double resault = total - (discount * total) / 100;
+    return resault;
+}
